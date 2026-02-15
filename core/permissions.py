22ca21c4d480408e4e97049047d15991ebbd598c
emmaroa <189168186+emmaroa@users.noreply.github.com>
2026-02-15 10:25:52 -0700

    Delete core directory

--- core/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.groups.filter(name='admin').exists()
-
-class IsCapturista(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.groups.filter(name__in=['capturista','admin']).exists()
